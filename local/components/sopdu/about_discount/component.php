5b72350651f6d023c4767ce3f210583dd47e9353
sopdu <info@sopdu.ru>
2021-06-09 19:41:03 +0300

    компонет подписки

--- local/components/sopdu/about_discount/component.php
+++ local/components/sopdu/about_discount/component.php
@@ -1,8 +1,45 @@
 <?php
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
-if(!empty($_POST)){
-    echo '<pre>'; print_r($_POST); '</pre>';
+if(!empty($_SESSION["sopdu"]["aboutDiscount"]["email"])){
+    echo GetMessage("sopduAboutDiscountTemplateSessionEmail");
 }
-
+if(!empty($_POST["624afcde4919170f71516ad0bde9ac84"])){
+    if(strpos($_POST["624afcde4919170f71516ad0bde9ac84"], '@') !== false){
+        CModule::IncludeModule("iblock");
+        $zapros = CIBlockElement::GetList(
+            [],
+            [
+                "IBLOCK_ID" =>  1,
+                "NAME"      =>  $_POST["624afcde4919170f71516ad0bde9ac84"]
+            ],
+            false,
+            false,
+            ["ID"]
+        );
+        if($zapros->fetch()){
+            $emailBlock = 'Y';
+        } else {
+            $emailBlock = 'N';
+        }
+        if(!empty($_SESSION["sopdu"]["aboutDiscount"]["email"])){
+            echo GetMessage("sopduAboutDiscountTemplateSessionEmail");
+        }
+        if($emailBlock == 'N'){
+            $_SESSION["sopdu"]["aboutDiscount"]["email"] = $_POST["624afcde4919170f71516ad0bde9ac84"];
+            $element = new CIBlockElement;
+            $element->Add([
+                "IBLOCK_ID" =>  1,
+                "NAME"      =>  $_POST["624afcde4919170f71516ad0bde9ac84"]
+            ]);
+        } else {
+            if(empty($_SESSION["sopdu"]["aboutDiscount"]["email"])) {
+                echo GetMessage("sopduAboutDiscountTemplateSessionEmail");
+            }
+        }
+    } else {
+        echo GetMessage("sopduAboutDiscountTemplateError");
+    }
+    unset($_POST["624afcde4919170f71516ad0bde9ac84"]);
+}
 $this->IncludeComponentTemplate();
 ?>
--- local/components/sopdu/about_discount/templates/.default/template.php
+++ local/components/sopdu/about_discount/templates/.default/template.php
@@ -18,15 +18,15 @@ $this->setFrameMode(true);
     <form class="footer-content-top__form-container" method="post">
         <div class="footer-content-top__form-input-wrap">
             <div class="footer-content-top__form-input-wrap-placeholder">Email</div>
-            <input type="text" class="footer-content-top__form-input" placeholder="">
-            <div class="footer-content-top__form-input-icon">
+            <input type="text" class="footer-content-top__form-input" name="624afcde4919170f71516ad0bde9ac84" placeholder="">
+            <button type="submit" class="footer-content-top__form-input-icon">
                 <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M26.25 6.5625L15 16.875L3.75 6.5625" stroke="#D5D7DC" stroke-linecap="round" stroke-linejoin="round"/>
                     <path d="M3.75 6.5625H26.25V22.5C26.25 22.7486 26.1512 22.9871 25.9754 23.1629C25.7996 23.3387 25.5611 23.4375 25.3125 23.4375H4.6875C4.43886 23.4375 4.2004 23.3387 4.02459 23.1629C3.84877 22.9871 3.75 22.7486 3.75 22.5V6.5625Z" stroke="#D5D7DC" stroke-linecap="round" stroke-linejoin="round"/>
                     <path d="M12.9545 15L4.03906 23.1726" stroke="#D5D7DC" stroke-linecap="round" stroke-linejoin="round"/>
                     <path d="M25.961 23.1726L17.0454 15" stroke="#D5D7DC" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
-            </div>
+            </button>
         </div>
     </form>
 </div>
